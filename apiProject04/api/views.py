7740fce9845ed5bc37330625065b8dfd6cb315af
Mohit Kumar <mks.djcet@gmail.com>
2025-11-03 14:58:42 +0530

    Updated code

--- apiProject04/api/views.py
+++ apiProject04/api/views.py
@@ -16,22 +16,48 @@
 #     return Response({"message": f"Hello, {request.user.username}. This is a private view accessible only to authenticated users."})
 
 
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from .models import Blog
+# from .serializers import BlogSerializer
+# from rest_framework import status
+
+# @api_view(['GET', 'POST'])
+# def blog_list(request):
+#     if request.method == 'GET':
+#         blogs = Blog.objects.all()
+#         serializer = BlogSerializer(blogs, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = BlogSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from .models import Blog
-from .serializers import BlogSerializer
-from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 
-@api_view(['GET', 'POST'])
-def blog_list(request):
-    if request.method == 'GET':
-        blogs = Blog.objects.all()
-        serializer = BlogSerializer(blogs, many=True)
-        return Response(serializer.data)
+#api Only for authenticated users
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    user = request.user
+    return Response({
+        "username": user.username,
+        "email": user.email,
+        "is_staff": user.is_staff,
+    })
 
-    elif request.method == 'POST':
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#api Only for admin users
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAdminUser])
+def admin_panel(request):
+    return Response({"message": f"Welcome to the admin panel, {request.user.username}."})
--- apiProject04/apiProject04/settings.py
+++ apiProject04/apiProject04/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -126,9 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST Framework Configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
