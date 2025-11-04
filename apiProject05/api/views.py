e92564db3871533557f44aedb49978931871e9cc
Mohit Kumar <mks.djcet@gmail.com>
2025-11-04 14:02:53 +0530

    Updated code

---
+++ apiProject05/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def post_list(request):
+    if request.method == 'GET':
+        return Response({"message": "Public can view this data"})
+    elif request.method == 'POST':
+        return Response({"message": f"Data created by {request.user.username}"})
